0179a87fa501dbea00f423ddc92b120fabf0231b
Tom Wilson <tom@jackhq.com>
2010-07-25 07:57:35 -0400

    add page indicator

--- wwwroot/javascripts/application.coffee
+++ wwwroot/javascripts/application.coffee
@@ -1,6 +1,13 @@
+# Remember to remove application.js for coffee to complie
 $( ->
   $('#sidebar li').prepend('''
     <span class="ui-icon ui-icon-circle-arrow-e" style="float:left;margin-right: 0.3em;"></span>
   ''')
   $('#sidebar ul').addClass('ui-corner-all').show('slide',{ direction: 'right'},'slow')
+  page: $('#page').text() || 'mission'
+  if page == 'mission'
+    $('a[href="/"]').css('color','blue')  
+  else
+    $('a[href*="' + page + '"]').css('color','blue')
+
 )
--- wwwroot/javascripts/application.js
+++ wwwroot/javascripts/application.js
@@ -1,8 +1,16 @@
 (function(){
+  // Remember to remove application.js for coffee to complie
   $(function() {
+    var page;
     $('#sidebar li').prepend('<span class="ui-icon ui-icon-circle-arrow-e" style="float:left;margin-right: 0.3em;"></span>');
-    return $('#sidebar ul').addClass('ui-corner-all').show('slide', {
+    $('#sidebar ul').addClass('ui-corner-all').show('slide', {
       direction: 'right'
     }, 'slow');
+    page = $('#page').text() || 'mission';
+    if (page === 'mission') {
+      return $('a[href="/"]').css('color', 'blue');
+    } else {
+      return $('a[href*="' + page + '"]').css('color', 'blue');
+    }
   });
 })();
